51dd9bf9186aede2764cd3ad097eb27729e78f40
jiujuanfeng <jiujuanfeng@163.com>
2019-01-25 16:35:08 +0800

    db

--- db/db.go
+++ db/db.go
@@ -1,26 +1,26 @@
 package db
 
 import (
-	"fmt"
-	log "github.com/Sirupsen/logrus"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/jiujuanfeng/elasticron/config"
+    "fmt"
+    log "github.com/Sirupsen/logrus"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+    "github.com/jiujuanfeng/elasticron/config"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	mysqlConf := config.BlogConfig.Mysql
-	dsn := fmt.Sprintf("%s:%s@/%s?charset=%s", mysqlConf.Username, mysqlConf.Password, mysqlConf.DB, mysqlConf.Charset)
-	db, err := gorm.Open("mysql", dsn)
-	if err != nil {
-		log.Errorf("open db error: %s", err.Error())
-	}
-	db.DB().SetMaxIdleConns(mysqlConf.MaxIdleConn)
-	db.DB().SetMaxOpenConns(mysqlConf.MaxOpenConn)
-	db.SingularTable(true)
-	db.LogMode(true)
+    mysqlConf := config.BlogConfig.Mysql
+    dsn := fmt.Sprintf("%s:%s@/%s?charset=%s", mysqlConf.Username, mysqlConf.Password, mysqlConf.DB, mysqlConf.Charset)
+    db, err := gorm.Open("mysql", dsn)
+    if err != nil {
+        log.Errorf("open db error: %s", err.Error())
+    }
+    db.DB().SetMaxIdleConns(mysqlConf.MaxIdleConn)
+    db.DB().SetMaxOpenConns(mysqlConf.MaxOpenConn)
+    db.SingularTable(true)
+    db.LogMode(true)
 
-	DB = db
+    DB = db
 }
